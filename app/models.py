34ea47734cfbdd237869112b6c5ebe620a6226c6
anlan <anlan1996@qq.com>
2018-09-08 11:46:20 +0800

    简单注册与登陆

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from _md5 import md5
+
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -25,6 +27,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 
 @login.user_loader
