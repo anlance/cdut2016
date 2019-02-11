2e7f21afac9bc3ffc6a57c74e859f77e1ac26095
anlan <anlan1996@qq.com>
2019-02-11 21:10:30 +0800

    v1.1

--- cdut2016.py
+++ cdut2016.py
@@ -7,10 +7,9 @@ from app.news_cdut.models import NewsCdut
 app = create_app()
 app.app_context().push()
 # 启动后台任务
-# scheduler.add_job(func='app.task:init', args='', next_run_time=datetime.datetime.now(), id='init')
-# scheduler.add_job(func='app.task:update_cdut', args='', trigger='interval', seconds=1200, id='update')
-scheduler.add_job(func='app.task:update_score', args='', trigger='interval', seconds=60, id='update_2')
-# scheduler.add_job(func='app.task:init_cdut', args='', trigger='interval', seconds=36000, id='cdut_news')
+scheduler.add_job(func='app.task:init', args='', next_run_time=datetime.datetime.now(), id='init')
+scheduler.add_job(func='app.task:update_score', args='', trigger='interval', seconds=600, id='update_2')
+scheduler.add_job(func='app.task:init_cdut', args='', trigger='interval', seconds=36000, id='cdut_news')
 scheduler.start()
 app.run()
 
